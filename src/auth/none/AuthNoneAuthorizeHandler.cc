9997aca72abc001c02b6fb76c64ad58cd6ec8f88
Peter Reiher <reiher@inktank.com>
2012-07-23 12:09:02 -0700

    Added param to verify_authorizer() call in AuthNone
    Signed-off-by: Peter Reiher <reiher@inktank.com>

--- src/auth/none/AuthNoneAuthorizeHandler.cc
+++ src/auth/none/AuthNoneAuthorizeHandler.cc
@@ -19,7 +19,7 @@
 
 bool AuthNoneAuthorizeHandler::verify_authorizer(CephContext *cct, KeyStore *keys,
 						 bufferlist& authorizer_data, bufferlist& authorizer_reply,
-						 EntityName& entity_name, uint64_t& global_id, AuthCapsInfo& caps_info,
+						 EntityName& entity_name, uint64_t& global_id, AuthCapsInfo& caps_info, CryptoKey& session_key,
 uint64_t *auid)
 {
   bufferlist::iterator iter = authorizer_data.begin();
